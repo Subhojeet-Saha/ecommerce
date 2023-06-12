4a166aace280a5bf7bee9c23095d6f5486d7ab7d
unknown <Subhojeet.loco@gmail.com>
2023-06-12 21:01:40 +0530

    initial commit

--- client/src/componenets/layout/Header.js
+++ client/src/componenets/layout/Header.js
@@ -5,8 +5,8 @@ import {GiShoppingBag} from "react-icons/gi"
 const Header = () => {
   return (
    <>
-   <nav className="navbar navbar-expand-lg bg-body-tertiary">
-  <div className="container-fluid">
+   <nav className="navbar navbar-expand-sm bg-body-tertiary">
+    <div className="container-fluid">
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon" />
     </button>
